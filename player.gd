66af5efe28f297a7ea1450f1f80226324f9cd5d1
dadifr <dav.calise@stud.uniroma3.it>
2025-12-08 10:27:51 +0100

    movimento player

--- player.gd
+++ player.gd
@@ -1,10 +1,15 @@
-extends CharacterBody2D
+extends AnimatedSprite2D
 
 const SPEED = 130.0
 var v = Vector2( )
 
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
 
-func _physics_process(delta: float) -> void:
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	
 	#movimento 3D sulla mappa di gioco
 	
 	if Input.is_action_pressed("ui_up"):
