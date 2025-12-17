0705a38e62b406bd651789aa819a2fcbfaf409bc
dadifr <dav.calise@stud.uniroma3.it>
2025-12-17 16:39:10 +0100

    menu aggiunto
    tasto menu aggiunto alla schermata di GameOver

--- scripts/GameOverScreen.gd
+++ scripts/GameOverScreen.gd
@@ -2,11 +2,17 @@ extends CanvasLayer
 
 func _on_riprova_pressed() -> void:
 	# 1. Ricarica il gioco
-	get_tree().reload_current_scene()
+	get_tree().change_scene_to_file("res://scenes/world.tscn")
 	
 	# 2. Distruggi questa schermata di Game Over!
 	self.queue_free()
+# Tasto RIPROVA (Ricarica veloce stesso personaggio)
+
+func _on_menu_pressed() -> void:
 	
+	get_tree().change_scene_to_file("res://scenes/character_selection.tscn")
+	
+	self.queue_free()
 
 
 
