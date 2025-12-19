7d9c50d4cf111c23dfeaa128bab46f966bd540de
dadifr <dav.calise@stud.uniroma3.it>
2025-12-19 15:57:32 +0100

    new pg lizard
    nuovo pg creato, funzionante

--- scripts/character_selection.gd
+++ scripts/character_selection.gd
@@ -4,17 +4,20 @@ extends Control
 @export var knight_scene: PackedScene
 @export var elf_scene: PackedScene
 @export var dwarf_scene: PackedScene
+@export var lizard_scene: PackedScene
 
 # Riferimenti ai bottoni (se li hai chiamati diversamente, cambia i nomi qui!)
 @onready var btn_knight = $HBoxContainer/BtnCavaliere
 @onready var btn_elf = $HBoxContainer/BtnElfo
 @onready var btn_dwarf = $HBoxContainer/BtnNano
+@onready var btn_lizard = $HBoxContainer/BtnLizard
 
 func _ready():
 	# Colleghiamo i click dei bottoni
 	btn_knight.pressed.connect(_on_knight_selected)
 	btn_elf.pressed.connect(_on_elf_selected)
 	btn_dwarf.pressed.connect(_on_dwarf_selected)
+	btn_lizard.pressed.connect(_on_lizard_selected)
 
 func _on_knight_selected():
 	# 1. Diciamo al GameManager: "Ho scelto il Cavaliere!"
@@ -23,14 +26,17 @@ func _on_knight_selected():
 	start_game()
 
 func _on_elf_selected():
-	# 1. Diciamo al GameManager: "Ho scelto il Mago!"
 	GameManager.selected_character_scene = elf_scene
 	# 2. Carichiamo il livello di gioco
 	start_game()
 
 func _on_dwarf_selected():
-	# 1. Diciamo al GameManager: "Ho scelto il Cavaliere!"
 	GameManager.selected_character_scene = dwarf_scene
+	# 2. Carichiamo il livello di gioco
+	start_game()
+
+func _on_lizard_selected():
+	GameManager.selected_character_scene = lizard_scene
 	# 2. Carichiamo il livello di gioco
 	start_game()
 
