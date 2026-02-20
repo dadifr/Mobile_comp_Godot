43e8115ebc82f2be556195d175f69b19e81dea6e
Yur11323 <yur.digirolamo@stud.uniroma3.it>
2026-02-20 23:39:47 +0100

    magonero

--- scripts/laser.gd
+++ scripts/laser.gd
@@ -4,21 +4,28 @@ extends Node2D
 @onready var ray = $RayCast2D
 
 var is_charging = true
+var creator = null # Variabile per memorizzare chi ha sparato il laser
 
 func _ready():
-	# Traiettoria iniziale (puntamento)
+	# Memorizziamo chi ha creato il laser (il Mago o il Boss)
+	creator = get_parent() 
+	
 	line.width = 2.0
-	line.default_color = Color(1, 0, 0, 0.3) # Rosso tenue
-	# Inizializziamo i punti della linea
+	line.default_color = Color(1, 0, 0, 0.3)
 	line.clear_points()
 	line.add_point(Vector2.ZERO)
 	line.add_point(Vector2.ZERO)
 
 func _process(_delta):
-	# Mentre carica, aggiorna visivamente la linea di puntamento
 	if is_charging:
+		# --- CONTROLLO ANTI-RESTO FISSO ---
+		# Se il creatore non è più valido (ucciso), distruggi il laser
+		if not is_instance_valid(creator):
+			queue_free()
+			return
+
 		ray.force_raycast_update()
-		var beam_end = ray.target_position # Default se non colpisce nulla
+		var beam_end = ray.target_position
 		if ray.is_colliding():
 			beam_end = to_local(ray.get_collision_point())
 		
@@ -26,6 +33,7 @@ func _process(_delta):
 
 func fire_laser():
 	is_charging = false
+	# ... (il resto della funzione fire_laser rimane uguale) ...
 	ray.force_raycast_update()
 	
 	var final_beam_end = ray.target_position
@@ -35,13 +43,11 @@ func fire_laser():
 		if target.is_in_group("player") and target.has_method("take_damage"):
 			target.take_damage(1, global_position)
 
-	# --- EFFETTO SPARO ---
 	line.set_point_position(1, final_beam_end)
-	line.width = 18.0 # Laser bello grande
-	line.default_color = Color(1, 1, 1, 1) # Bianco/HDR
+	line.width = 18.0 
+	line.default_color = Color(1, 1, 1, 1) 
 	
 	var tween = create_tween()
-	# Effetto pulsazione e dissolvenza
 	tween.tween_property(line, "width", 28.0, 0.05)
 	tween.tween_property(line, "width", 0, 0.15)
 	tween.finished.connect(queue_free)
