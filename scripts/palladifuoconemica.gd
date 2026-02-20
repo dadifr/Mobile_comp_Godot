4780a712259a083375a0c3a9b41392d706b85d81
Yur11323 <yur.digirolamo@stud.uniroma3.it>
2026-02-20 12:00:17 +0100

    palla di fuoco demone

---
+++ scripts/palladifuoconemica.gd
@@ -0,0 +1,50 @@
+extends Area2D
+
+@export var speed = 400.0
+@export var damage = 1
+@export var life_time: float = 5.0 
+
+var direction = Vector2.RIGHT
+var shooter = null 
+
+@onready var anim = $AnimatedSprite2D
+
+func _ready():
+	# nascita
+	anim.play("creazione")
+	anim.animation_finished.connect(_on_animation_finished)
+	
+	#collisioni
+	body_entered.connect(_on_body_entered)
+	
+	#timer
+	get_tree().create_timer(life_time).timeout.connect(queue_free)
+
+func setup(new_dir, new_damage, who_shot):
+	direction = new_dir.normalized() # Usiamo normalized per sicurezza
+	damage = new_damage
+	shooter = who_shot
+	rotation = direction.angle() 
+	if direction.x < 0:
+		$AnimatedSprite2D.flip_v = true
+
+func _physics_process(delta):
+	# Movimento
+	position += direction * speed * delta
+
+func _on_animation_finished():
+	anim.play("movimento")
+
+func _on_body_entered(body):
+	if not is_instance_valid(body):
+		return
+	if body == shooter:
+		return
+	#danno
+	if body.has_method("take_damage"):
+		body.take_damage(damage, global_position)
+		print("Palla di fuoco a segno! Danno: ", damage)
+		queue_free()
+	# muro
+	elif body is TileMap or body is StaticBody2D or body is TileMapLayer:
+		queue_free()
